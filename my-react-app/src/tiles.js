117ccbe2b8f1eff304edb352f97b7f08b1938ef1
cindy <cindy.wu23236@gmail.com>
2026-03-15 17:57:27 -0700

    changed the tiles to be capitalized

--- my-react-app/src/tiles.js
+++ my-react-app/src/tiles.js
@@ -1,8 +1,8 @@
 export const tiles = {
   categories: [
-    { name: "Hunter, starts with C", color: "#f9df6d", words: ["CAPS", "COOKIES", "CAMERA", "CAUCASIAN"] },
-    { name: "Felicia", color: "#a0c35a", words: ["BELLY", "SWIMMER", "DADA", "FLYING"] },
-    { name: "Kelly, with an O in it", color: "#b0c4ef", words: ["GHOSTER", "KOREAN", "NO HAIR", "STORY"] },
-    { name: "___jin", color: "#ba81c5", words: ["FREAK", "LESBI", "OKAY", "FART"] },
+    { name: "HUNTER, STARTS WITH C", color: "#f9df6d", words: ["CAPS", "COOKIES", "CAMERA", "CAUCASIAN"] },
+    { name: "FELICIA", color: "#a0c35a", words: ["BELLY", "SWIMMER", "DADA", "FLYING"] },
+    { name: "KELLY, WITH AN O IN IT", color: "#b0c4ef", words: ["GHOSTER", "KOREAN", "NO HAIR", "STORY"] },
+    { name: "_____JIN", color: "#ba81c5", words: ["FREAK", "LESBI", "OKAY", "FART"] },
   ]
 }
